2f29a910bf0d1b6f0d0ed0f4fbef23d5b4a0cc30
eodn <s20063@gsm.hs.kr>
2021-08-24 19:52:15 +0900

    test

--- js/main.js
+++ js/main.js
@@ -43,7 +43,27 @@ const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
    //(요소,지속시간. 옵션);
   gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7,
+    delay: (index + 1) * .7, 
     opacity: 1
   });
+});
+
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+   autoplay: true,
+   loop: true
+
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  }
+  
+
 });
