7d91db6c83316a342e6d531f8603a42938460085
eodn <s20063@gsm.hs.kr>
2021-08-30 09:01:13 +0900

    [test4]

--- js/main.js
+++ js/main.js
@@ -113,8 +113,8 @@ floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
 
 
-const spyEls = document.querySelectorAll('section.scoll-spy')
-spyEls.forEach(function (spyEls) {
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({
       triggerElement: spyEls, //보여짐 여부를 감시할 요소를 지정
